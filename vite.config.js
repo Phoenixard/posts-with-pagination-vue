94aefcd64d267430e894f3d18e1126b9ee24ec15
Eugene <phoen1xus2643@gmail.com>
2024-06-12 01:18:30 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/posts-with-pagination/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/my-pagination/' : '/',
   build: {
     outDir: 'docs',
 
